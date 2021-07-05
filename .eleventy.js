451fdd1e65a7d22f08975ec3a4cfa8e4ba607874
Melanie Richards <mr@melanie-richards.com>
2021-07-05 12:17:50 -0700

    Update social media-related fields and similar images

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function (eleventyConfig) {
 
   // Don't try to build asset files, just transparently copy them through
   eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addPassthroughCopy('manifest.webmanifest');
 
   // Insert SVG contents
   eleventyConfig.addPlugin(svgContents);
