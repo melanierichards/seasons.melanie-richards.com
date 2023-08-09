f514ebff51ca1f764e121d531046b6a804fbef87
Melanie Richards <mr@melanie-richards.com>
2023-08-08 21:21:28 -0700

    Replace time-triggered notifications with a calendar (fixes #5)

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-// Open the website on notification click
-self.addEventListener('notificationclick', event => {
-  if (event.action === 'close') {
-    event.notification.close();
-  } else {
-    self.clients.openWindow('/');
-  }
-});
